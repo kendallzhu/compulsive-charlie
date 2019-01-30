7a0abb6b7421d88f80c3a98805161df3c2d2f076
kendallzhu <kzhu@college.harvard.edu>
2019-01-30 17:20:20 -0500

    fixed more edge detection bugs
    added abovegroundright transform in player

--- Compulsive Charlie/Assets/Scripts/Player/PlayerController.cs
+++ Compulsive Charlie/Assets/Scripts/Player/PlayerController.cs
@@ -11,11 +11,12 @@ public class PlayerController : MonoBehaviour {
     public const float forwardJumpForce = 100f;
     public const float fallingMinForwardSpeed = .5f; // idea: maybe can make this a profile upgrade?
 
-    public Transform groundCheck1;
-    public Transform groundCheck2;
-    public bool grounded = false;
-    public bool nearEdge = false;
-    public bool nearEdgeLastFrame = false;
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+    public Transform aboveGroundRight;
+    public bool grounded;
+    public bool nearEdge;
+    public bool nearEdgeLastFrame;
     // private Animator anim;
     private Rigidbody2D rb2d;
     private RunManager runManager;
@@ -52,8 +53,8 @@ public class PlayerController : MonoBehaviour {
         }
 
         // check for grounded
-        grounded = Physics2D.Linecast(transform.position, groundCheck1.position, 1 << LayerMask.NameToLayer("Ground")) ||
-            Physics2D.Linecast(transform.position, groundCheck2.position, 1 << LayerMask.NameToLayer("Ground"));
+        grounded = Physics2D.Linecast(transform.position, groundCheckLeft.position, 1 << LayerMask.NameToLayer("Ground")) ||
+            Physics2D.Linecast(transform.position, groundCheckRight.position, 1 << LayerMask.NameToLayer("Ground"));
 
         // increase jump by spending energy (on tap)
         if (Input.GetButtonDown("Jump") && grounded && runState.energy > 0)
@@ -78,11 +79,9 @@ public class PlayerController : MonoBehaviour {
 
         // auto-activate jump callback sequence when first near edge of platform
         nearEdgeLastFrame = nearEdge;
-        Vector2 forwardPosHigh = new Vector2(transform.position.x + .5f, transform.position.y);
-        Vector2 forwardPosLow = new Vector2(transform.position.x + .6f, transform.position.y - 2);
-        nearEdge = !Physics2D.Linecast(forwardPosHigh, forwardPosLow, 1 << LayerMask.NameToLayer("Ground"));
+        nearEdge = !Physics2D.Linecast(aboveGroundRight.position, groundCheckRight.position, 1 << LayerMask.NameToLayer("Ground"));
         // runManager.PreJump => thoughtMenu.Activate => runManager.PostThoughtSelect => player.Jump
-        if (nearEdge && !nearEdgeLastFrame)
+        if (grounded && nearEdge && !nearEdgeLastFrame)
         {
             runManager.PreJump();
         }
