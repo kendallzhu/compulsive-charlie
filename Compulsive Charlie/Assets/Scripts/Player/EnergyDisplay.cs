43294812fa7852c4e572258a6b336d539d93d5cf
kendallzhu <kzhu@college.harvard.edu>
2019-02-22 20:41:48 -0500

    bare bones energy bar
    stuff getting hacky yikes

--- Compulsive Charlie/Assets/Scripts/Player/EnergyDisplay.cs
+++ Compulsive Charlie/Assets/Scripts/Player/EnergyDisplay.cs
@@ -6,18 +6,29 @@ using TMPro;
 // script for graphic display of energy - right now just text but hopefully will be nicer
 public class EnergyDisplay : MonoBehaviour
 {
+    public float maxBarSize;
+    public float barThickness;
+
     public RunManager runManager;
+    public GameManager gameManager;
 
     // Initialization
     void Awake()
     {
         // get reference to runManager
         runManager = Object.FindObjectOfType<RunManager>();
+        // get reference to gameManager
+        gameManager = Object.FindObjectOfType<GameManager>();
     }
 
     void Update()
     {
         RunState runState = runManager.runState;
-        gameObject.GetComponent<TextMeshProUGUI>().text = runState.energy.ToString();
+        transform.Find("Text").GetComponent<TextMeshProUGUI>().text = runState.energy.ToString();
+        // set bar
+        float maxEnergy = gameManager.profile.energyCap;
+        float energy = runManager.runState.energy;
+        float size = maxBarSize * energy / maxEnergy;
+        transform.Find("Bar").localScale = new Vector3(size, barThickness, 1f);
     }
 }
