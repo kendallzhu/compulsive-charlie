afff5a21cd385d825f76b90080186448cb6ea8bf
jna0108 <jna0108@gmail.com>
2019-03-10 00:01:16 -0500

    Rhythm Minigame

---
+++ Compulsive Charlie/Assets/Scripts/Rhythm/Clock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+    float startTime;
+
+	void Start()
+    {
+        restartClock();
+	}
+
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            restartClock();
+        }
+	}
+
+    void restartClock()
+    {
+        startTime = Time.time;
+    }
+
+    public float getTime()
+    {
+        return Time.time - startTime;
+    }
+}
---
+++ Compulsive Charlie/Assets/Scripts/Rhythm/NoteManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public List<GameObject> notes;
+    public List<float> timings;
+
+    public float window; // how generous should we be
+
+    public void DestroyNote()
+    {
+        Destroy(notes[0]);
+        notes.RemoveAt(0);
+        timings.RemoveAt(0);
+    } 
+}
