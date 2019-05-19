0167b3941601e240cc76bb965c3bb6d32ba31a27
kendallzhu <kzhu@college.harvard.edu>
2019-05-18 23:09:48 -0400

    Turn on rain when sad, lightning when anxious
    more to think about

--- Compulsive Charlie/Assets/Scripts/Background/BackgroundManager.cs
+++ Compulsive Charlie/Assets/Scripts/Background/BackgroundManager.cs
@@ -13,17 +13,27 @@ public class BackgroundManager : MonoBehaviour
     public GameObject bgTile;
     public GameObject tileBase;
     public GameObject tileRain;
-    // public GameObject tileMORESTUFF;
+    public GameObject tileLightning;
+
+    private RunManager runManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // get reference to runManager
+        runManager = Object.FindObjectOfType<RunManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RunState runState = runManager.runState;
+        EmotionState e = runState.emotions;
+        // rain if sad
+        tileRain.SetActive(e.Extremeness(EmotionType.despair) > 1);
+        // lighning if anxious
+        tileLightning.SetActive(e.Extremeness(EmotionType.anxiety) > 1);
+        // send the emotion level into the tile base
+        tileBase.GetComponent<Animator>().SetInteger("emotionTotal", e.GetSum());
     }
 }
